dedbf3c23339ea38ae80e429f18acdb3f64c22cb
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-08-10 19:28:31 +0530

    added toaster

--- front-end/src/Remainder/Remainder.js
+++ front-end/src/Remainder/Remainder.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../index.css"; // Make sure to import your global CSS here
+require("dotenv").config({ path: "../../.env.local" });
 
 const AppointmentForm = () => {
   const [appointment, setAppointment] = useState({
@@ -13,8 +16,9 @@ const AppointmentForm = () => {
     preferredMethod: "Email",
     vaccinationType: "",
   });
-  console.log(appointment)
-  const port = process.env.REACT_APP_API_PORT || 3001;
+//   console.log(appointment);
+  const port = process.env.REACT_APP_API_PORT ||3001;
+//   console.log(port)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,14 +28,25 @@ const AppointmentForm = () => {
       return;
     }
 
-    console.log(appointment);
+    // console.log(appointment);
 
     // Send the appointment data to the server
     axios
       .post(`http://localhost:${port}/appointment`, appointment)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
+      toast.success("Thanks, we will remind you before 24hrs of vaccination!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
   };
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,94 +54,108 @@ const AppointmentForm = () => {
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit} className="appointment-form">
-        <div className="form-field">
-          <label className="label">Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={appointment.name}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Date of Birth:</label>
-          <input
-            type="date"
-            name="dob"
-            value={appointment.dob}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Contact:</label>
-          <input
-            type="text"
-            name="contact"
-            value={appointment.contact}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Preferred Method of Communication:</label>
-          <select
-            name="preferredMethod"
-            value={appointment.preferredMethod}
-            onChange={handleChange}
-            className="input"
-          >
-            <option value="Email">Email</option>
-            <option value="SMS">SMS</option>
-          </select>
-        </div>
-        <div className="form-field">
-          <label className="label">Vaccination Type:</label>
-          <input
-            type="text"
-            name="vaccinationType"
-            value={appointment.vaccinationType}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Vaccination Date:</label>
-          <input
-            type="date"
-            name="date"
-            value={appointment.date}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Vaccination Time:</label>
-          <input
-            type="time"
-            name="time"
-            value={appointment.time}
-            onChange={handleChange}
-            className="input"
-          />
-        </div>
-        <div className="form-field">
-          <label className="label">Additional Notes/Comments:</label>
-          <textarea
-            name="description"
-            value={appointment.description}
-            onChange={handleChange}
-            className="input"
-            rows="3"
-          />
-        </div>
-        <button type="submit" className="submit-button">
-          Add Appointment
-        </button>
-      </form>
+    <div className="contactPage">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className="form-container">
+        <form onSubmit={handleSubmit} className="appointment-form">
+          <div className="form-field">
+            <label className="label">Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={appointment.name}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Date of Birth:</label>
+            <input
+              type="date"
+              name="dob"
+              value={appointment.dob}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Contact:</label>
+            <input
+              type="text"
+              name="contact"
+              value={appointment.contact}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Preferred Method of Communication:</label>
+            <select
+              name="preferredMethod"
+              value={appointment.preferredMethod}
+              onChange={handleChange}
+              className="input"
+            >
+              <option value="Email">Email</option>
+              <option value="SMS">SMS</option>
+            </select>
+          </div>
+          <div className="form-field">
+            <label className="label">Vaccination Type:</label>
+            <input
+              type="text"
+              name="vaccinationType"
+              value={appointment.vaccinationType}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Vaccination Date:</label>
+            <input
+              type="date"
+              name="date"
+              value={appointment.date}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Vaccination Time:</label>
+            <input
+              type="time"
+              name="time"
+              value={appointment.time}
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+          <div className="form-field">
+            <label className="label">Additional Notes/Comments:</label>
+            <textarea
+              name="description"
+              value={appointment.description}
+              onChange={handleChange}
+              className="input"
+              rows="3"
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Add Appointment
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
